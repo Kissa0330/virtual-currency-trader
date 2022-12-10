7b01fe35bdbc63720c088eab62d2022fc8273311
らいか <raika4789@gmail.com>
2022-12-10 19:01:03 +0900

    仮完成

--- main.js
+++ main.js
@@ -2,20 +2,76 @@ import config from "./config.js";
 import ccxt from "ccxt";
 
 const bitflyer = new ccxt.bitflyer(config);
+const orderSize = 0.0003; //約500円
 let record = [];
-
-await getData(bitflyer);
-tradeBitcoin();
-await sleep(10000);
+let order = null;
 
 const sleep = (waitTime) =>
   new Promise((resolve) => setTimeout(resolve, waitTime));
 
 async function getData(bitflyer) {
   const ticker = await bitflyer.fetchTicker("FX_BTC_JPY");
-  record.push(ticker);
+  record.push(ticker.last);
   while (record.length > 10) record.shift();
+  return record;
 }
 
-function tradeBitcoin() {
+async function tradeBitcoin(bitflyer, order, record) {
+  if (order) {
+    if (isSell(record)) {
+      order = await bitflyer.createMarketSellOrder("FX_BTC_JPY", orderSize);
+      console.log("sell order:" + order);
+      return null;
+    } else if (isLosscut(record)) {
+      order = await bitflyer.createMarketSellOrder("FX_BTC_JPY", orderSize);
+      console.log("loss cut order:" + order);
+      return null;
+    }
+  } else {
+    if (isBuy(record)) {
+      order = await bitflyer.createMarketBuyOrder("FX_BTC_JPY", orderSize);
+      console.log("buy order:" + order);
+      return order;
+    }
+  }
+  return null;
+}
+
+function isSell(record) {
+  let cnt = 0;
+
+  for (let i = 0; i < record.length - 1; i++) {
+    if (record[i] < record[i + 1]) cnt++;
+  }
+  if (cnt >= 8) return true;
+  else return false;
+}
+
+function isLosscut(record) {
+  let cnt = 0;
+
+  for (let i = 0; i < record.length - 1; i++) {
+    if (record[i] > record[i + 1]) cnt++;
+  }
+  if (cnt >= 10) return true;
+  else return false;
+}
+
+function isBuy(record) {
+  let cnt = 0;
+
+  for (let i = 0; i < record.length - 1; i++) {
+    if (record[i] > record[i + 1]) cnt++;
+  }
+  if (cnt >= 8) return true;
+  else return false;
+}
+while (1) {
+  record = await getData(bitflyer, record);
+  if (record.length == 10) {
+    console.log("record:" + record[9]);
+    order = await tradeBitcoin(bitflyer, order, record);
+    console.log("order:" + order);
+    await sleep(10000);
+  }
 }
